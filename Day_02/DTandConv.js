c1351d878e27dcacff3443211fe2d253b7d9e56a
DevJai-563 <jdsrdx@gmail.com>
2024-12-05 15:52:25 +0530

    Data Type and Conversion

---
+++ Day_02/DTandConv.js
@@ -0,0 +1,53 @@
+// Data Type and Conversion
+
+//  String into Number
+//1.converting name
+let name = "suresh" //string
+console.log("1.Name Type = ",typeof name);
+let a = Number(name)
+console.log("name is now = ",a,"|Type is ",typeof a);
+
+//2.converting pass
+let pass = "12ab"// string
+console.log("2.Pass nType = ",typeof pass);
+let b = Number(pass)
+console.log("pass is now = ",b,"|Type is ",typeof b);
+
+//3.converting how
+let how = ""  //string
+console.log("3.How = ",typeof how);
+let c = Number(how)
+console.log("how is now = ",c,"|Type is ",typeof c);
+
+//4.converting roll
+let roll = "332" //string
+console.log("4.Roll = ",typeof roll);
+let g = Number(roll)
+console.log("Roll is now = ",g,"|Type is ",typeof g);
+
+//5.Number into String
+let age = 25 //number
+console.log("5.Age Type = ",typeof age);
+let d = String(age)
+console.log("age is now = ",d,"|Type is ",typeof d);
+
+//6.Numbers into Boolean
+let child = 1 //number
+console.log("6.Child = ",typeof child);
+let e = Boolean(child)
+console.log("child is now = ",e,"|Type is ",typeof e);
+
+//7.Boolean into Number
+let InActive = true // boolean
+console.log("7.InActive Type = ",typeof InActive);
+let f = Number(InActive)
+console.log("InActive is now = ",f,"|Type is ",typeof f);
+
+
+let ans = null //object
+console.log("Ans = ",typeof ans);
+
+let wrk  //undefines
+console.log("wrk = ",typeof wrk);
+
+
